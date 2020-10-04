4de7b62d285288b16056f9769dd9de5f8ba1c7c5
Nishanth Kumar <pathinishanth@gmail.com>
2020-10-04 20:09:25 +0300

    submit worker

--- Module 00 Getting Started/k8s-worker.sh
+++ Module 00 Getting Started/k8s-worker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 UBUNTU_VERSION=18.04
-node_type=master
+node_type=worker
 
 echo "Ubuntu version: ${UBUNTU_VERSION}"
 echo "K8s node type: ${node_type}"
@@ -52,39 +52,10 @@ sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni
 
 sudo apt-mark hold kubelet kubeadm kubectl kubernetes-cni
 
-#Initialize the k8s cluster
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16
-
-sleep 60
-
-#Create .kube file if it does not exists
-mkdir -p $HOME/.kube
-
-#Move Kubernetes config file if it exists
-if [ -f $HOME/.kube/config ]; then
-    mv $HOME/.kube/config $HOME/.kube/config.back
-fi
-
-sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-#if you are using a single node which acts as both a master and a worker
-#untaint the node so that pods will get scheduled:
-#kubectl taint nodes --all node-role.kubernetes.io/master-
-
-#Install Flannel network
-#kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml
-
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
-
 
 echo "Done."
 
-echo "Installation of Kubernetes Master has completed"
+echo "Installation of Kubernetes Worker has completed"
 echo "*************************************************"
-echo "Dont Close the Terminal, however observe the last few lines of output"
 
-kubeadm config print join-defaults
-
-echo "You are supposed to copy the join command and run on the worker nodes."
+echo "You are supposed to copy the join command from master and run on the worker nodes."
